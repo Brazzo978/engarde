c538b5ef018346e7bf5fb9f315ebe3119dde868d
Manuel Biagini <55558507+Brazzo978@users.noreply.github.com>
2025-08-01 15:58:24 +0200

    luci: add server and auth options

--- Owrt app Source/luci-app-engarde-easyconf/luasrc/model/cbi/engardeconf/engardeconf.lua
+++ Owrt app Source/luci-app-engarde-easyconf/luasrc/model/cbi/engardeconf/engardeconf.lua
@@ -2,8 +2,14 @@ config = Map("engardeconf")
 
 view = config:section(NamedSection,"Setup", "config",  translate("Engarde Configuration (cloud-init)"))
 enabled = view:option(Flag, "enabled", "Enable", "Enables Engarde and Wireguard, disabling Speedify and TinyFEC VPN.<br>Wait 20-30 seconds for Engarde & UI change to commence."); view.optional=false; view.rmempty = false;
-pass = view:option(Value, "pass", "Password:", "The password set in cloud-init password field."); pass.optional=false; pass.rmempty = false; pass.password=true;
-server = view:option(Value, "dstAddr", "Server IP Address:", "The server IP(v4), found in provider instance info.");
+pass = view:option(Value, "pass", "Password:", "Password for Engarde Web Manager.");
+pass.optional=false; pass.rmempty = false; pass.password=true;
+server = view:option(Value, "dstAddr", "Server Address:",
+    "IP address or hostname of the Engarde server.");
+server.optional=false; server.rmempty = false;
+user = view:option(Value, "username", "Username:",
+    "Username for Engarde Web Manager.");
+user.optional=false; user.rmempty = false;
 
 function config.on_commit(self)
     luci.sys.exec("sh -c 'sleep 2 && /etc/init.d/engardeconf restart' &")
